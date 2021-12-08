e2a61a71c6b255138e3b452c3d26274e7bbb17d8
hoangdinhhoi <hoangdinhhoi@vnpt.vn>
2021-12-08 13:53:00 +0700

    change version

---
+++ HiddenFramework/Dummyfile.swift
@@ -0,0 +1,16 @@
+//
+//  Dummyfile.swift
+//  HiddenFramework
+//
+//  Created by Gin on 08/12/2021.
+//
+
+import Foundation
+
+public func printLog(msg: String) {
+    print("Log: \(msg)")
+}
+
+public func printSuccess() {
+    print("This print success of HiddenFramework - by HoiHD")
+}
